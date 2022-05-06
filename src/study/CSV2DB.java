1b14bb897fc44df8d2f63d07d45e551a389084d8
mengxiangtai <wei5899584@gmail.com>
2022-05-06 14:32:38 +0900

    push

---
+++ src/study/CSV2DB.java
@@ -0,0 +1,81 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * CSVファイルを読み込み、DBに登録する
+ *
+ * @author c
+ * @version 1.0.0
+ */
+public class CSV2DB {
+
+	public static void main(String[] args) throws SecurityException, IOException {
+		/**
+		 * ログの初期処理 ログレベル設定
+		 */
+		Logger logger = Logger.getLogger("log-Test");
+		logger.setLevel(Level.ALL);
+		Date logDate = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MMddHHmm");
+		FileHandler fileHandler = new FileHandler("C:\\Users\\dbserver\\Desktop/Log"+dateFormat.format(logDate)+".log");
+		logger.addHandler(fileHandler);
+		fileHandler.setLevel(Level.ALL);
+		Formatter formatter = new SimpleFormatter();
+		fileHandler.setFormatter(formatter);
+		try {
+			logger.info("処理を開始しました。");
+			// JDBCドライバをロード
+			Class.forName("org.postgresql.Driver");
+			// DBに接続
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb", "postgres",
+					"root");
+			// コネクションの取得
+			Statement stmt = conn.createStatement();
+
+			// CSVファイルを読み込む
+			try {
+				File f = new File("infile.csv");
+				BufferedReader br = new BufferedReader(new FileReader(f));
+				int countNum = 0;
+				String line;
+				String strSql;
+				// SQLの実行
+				while ((line = br.readLine()) != null) {
+					String[] data = line.split(",", 0);
+					countNum++;
+					strSql="INSERT INTO outfile(pid,picname,rid,pictxt,ord) VALUES (" + data[0] + ","
+							+ data[1] + "," + data[2] + "," + data[3] + "," + data[4] + ");";
+//					strSql="INSERT INTO outfile(pid,picname,rid,pictxt,ord) VALUES (18,'cp',11,'20101011.jpg',1);";
+					stmt.executeUpdate(strSql);
+				}
+				logger.info(countNum+"件データが読み取りました。");
+				br.close();
+				logger.info("正常に終了しました。");
+			} catch (IOException e) {
+				logger.log(Level.SEVERE, "ファイルの読み取りに失敗しました", e);
+			}
+			// ステートメントとコレクションのclose
+			stmt.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.log(Level.WARNING, "エラーが発生しました。", e);
+			System.out.println(e);
+		}
+	}
+
+}
